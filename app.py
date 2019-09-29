26c0678b7a985df194ba93aa092a99637b7d4dfc
Farsheed Ashouri <farsheed.ashouri@gmail.com>
2019-09-29 12:39:44 +0300

    slack redirect edit

--- app.py
+++ app.py
@@ -46,8 +46,7 @@ app.config["SLACK_OAUTH_CLIENT_ID"] = os.environ.get('SLACK_CLIENT_ID')
 app.config["SLACK_OAUTH_CLIENT_SECRET"] = os.environ.get('SLACK_CLIENT_SECRET')
 
 slack_bp = make_slack_blueprint(
-    scope=["identify,bot,commands,channels:read,chat:write:bot,links:read,users:read,groups:read,im:read"],
-    redirect_url="/datasources")
+    scope=["identify,bot,commands,channels:read,chat:write:bot,links:read,users:read,groups:read,im:read"])
 slack_bp.authorized = authorized
 app.register_blueprint(slack_bp, url_prefix="/login")
 app.register_blueprint(google_auth.app)
