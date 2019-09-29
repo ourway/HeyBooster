9e2b0220a3de3f31a32ab004c57ddbaea6ffb203
Farsheed Ashouri <farsheed.ashouri@gmail.com>
2019-09-29 09:19:46 +0300

    test updated

--- app.py
+++ app.py
@@ -60,30 +60,42 @@ def get_image(pid):
 
 
 @app.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
     if 'auth_token' in session.keys() and 'sl_accesstoken' in session.keys():
         return redirect('/datasourcesinfo')
     elif 'auth_token' in session.keys() and not 'sl_accesstoken' in session.keys():
-        return render_template('index.html')
+        analytics_confirm = False
+        slack_confirm = False
+
+        try:
+            useraccount = google_analytics.get_accounts(session['email'])['accounts']
+            if useraccount:
+                analytics_confirm = True
+            if 'sl_accesstoken' in session.keys():
+                slack_confirm = True
+        except:
+            return render_template('test.html', slack_confirm=slack_confirm, analytics_confirm=analytics_confirm)
+        return render_template('test.html', slack_confirm=slack_confirm, analytics_confirm=analytics_confirm)
     else:
         return redirect('/login')
 
 
-@app.route('/test', methods=['GET', 'POST'])
-@login_required
-def test():
-    analytics_confirm = False
-    slack_confirm = False
-
-    try:
-        useraccount = google_analytics.get_accounts(session['email'])['accounts']
-        if useraccount:
-            analytics_confirm = True
-        if 'sl_accesstoken' in session.keys():
-            slack_confirm = True
-    except:
-        return render_template('test.html', slack_confirm=slack_confirm, analytics_confirm=analytics_confirm)
-    return render_template('test.html', slack_confirm=slack_confirm, analytics_confirm=analytics_confirm)
+# @app.route('/test', methods=['GET', 'POST'])
+# @login_required
+# def test():
+#     analytics_confirm = False
+#     slack_confirm = False
+#
+#     try:
+#         useraccount = google_analytics.get_accounts(session['email'])['accounts']
+#         if useraccount:
+#             analytics_confirm = True
+#         if 'sl_accesstoken' in session.keys():
+#             slack_confirm = True
+#     except:
+#         return render_template('test.html', slack_confirm=slack_confirm, analytics_confirm=analytics_confirm)
+#     return render_template('test.html', slack_confirm=slack_confirm, analytics_confirm=analytics_confirm)
 
 
 @app.route('/change', methods=['POST'])
