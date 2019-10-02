ff57497865d52947747048f262f79e23a2d7c587
altuntasmuhammet <altuntasmuhammet96@gmail.com>
2019-10-02 17:34:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1088,11 +1088,11 @@ def message_actions():
             if not 'image_url' in attachments[int(attachment_id) - 1]:
                 attachments[int(attachment_id) - 1]['image_url'] = imageurl.format(imageId)
                 for index in range(len(attachments[int(attachment_id) - 1]['actions'])):
-                    if(attachments[int(attachment_id) - 1]['actions'][1]['name']=='showgraph'):
+                    if(attachments[int(attachment_id) - 1]['actions'][index]['name']=='showgraph'):
                         attachments[int(attachment_id) - 1]['actions'][index]['text'] = "Close Graph"
             else:
                 for index in range(len(attachments[int(attachment_id) - 1]['actions'])):
-                    if(attachments[int(attachment_id) - 1]['actions'][1]['name']=='showgraph'):
+                    if(attachments[int(attachment_id) - 1]['actions'][index]['name']=='showgraph'):
                         del attachments[int(attachment_id) - 1]['image_url']
                         attachments[int(attachment_id) - 1]['actions'][index]['text'] = "Show Graph"
             
