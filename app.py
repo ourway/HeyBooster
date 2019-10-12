c3de5e85d607e202ecf25adcee8ed27d5e66e6de
altuntasmuhammet <altuntasmuhammet96@gmail.com>
2019-10-12 18:38:43 +0300

    Channel listing updated
    heybooster added as channel
    '@' symbol added to username

--- app.py
+++ app.py
@@ -230,11 +230,12 @@ def get_channels():
         imlist = requests.post(URL.format('im.list'), data).json()['ims']
 
         for user in userslist:
-            if (not user['is_bot']):
+            if (not user['is_bot'] or user['name'] == 'heybooster'):
                 for im in imlist:
                     if (user['id'] == im['user']):
                         #                    print("User ID:", user['id'], '\n', "User Name:", user['name'], '\n', "IM ID:", im['id'])
                         user['id'] = im['id']
+                        user['name'] = '@' + user['name']
                         channels += [user]
                         break
     except:
