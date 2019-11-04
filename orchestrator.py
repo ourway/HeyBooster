5b28df1b9b7073c91bfde2799b1466c48bd3018a
altuntasmuhammet <altuntasmuhammet96@gmail.com>
2019-11-04 15:04:28 +0300

    time sleep added to processes

--- orchestrator.py
+++ orchestrator.py
@@ -43,7 +43,7 @@ def do_job(tasks_to_accomplish):
             db.find_and_modify('notification', query={'email': task['email'], 'type': task['type']},
                                lastRunDate=time.time())
         except queue.Empty:
-            pass
+            time.sleep(59.5 - datetime.now().second)
         except Exception as ex:
             logging.error(f"TASK DID NOT RUN --- User Email: {user['email']} Data Source ID: {task['datasourceID']} Task Type: {task['type']} --- {str(ex)}")
     return True
