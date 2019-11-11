d91beb2f87381321bf079663bec0b388ae743577
Farsheed Ashouri <farsheed.ashouri@gmail.com>
2019-11-12 00:37:28 +0300

    test

--- app.py
+++ app.py
@@ -512,7 +512,6 @@ def connectaccount():
             'email': session['email'],
             'sl_userid': uID,
             'sourceType': "Google Analytics",
-            'dataSourceName': nForm.data_source_name,
             'accountID': nForm.account.data.split('\u0007')[0],
             'accountName': nForm.account.data.split('\u0007')[1],
             'propertyID': nForm.property.data.split('\u0007')[0],
@@ -598,12 +597,13 @@ def getaudit():
     if request.method == 'POST':
         uID = db.find_one("user", query={"email": session["email"]})['sl_userid']
         ts = time.time()
+        print("----------------------------------------------")
+        print(nForm.data_source_name)
 
         data = {
             'email': session['email'],
             'sl_userid': uID,
             'sourceType': "Google Analytics",
-            'dataSourceName': nForm.data_source_name,
             'accountID': nForm.account.data.split('\u0007')[0],
             'accountName': nForm.account.data.split('\u0007')[1],
             'propertyID': nForm.property.data.split('\u0007')[0],
