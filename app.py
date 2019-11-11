8e407dc222e0ad0273f5ee1c3c3e01caa26bf944
Farsheed Ashouri <farsheed.ashouri@gmail.com>
2019-11-12 00:48:36 +0300

    added datasourcename input

--- app.py
+++ app.py
@@ -512,6 +512,7 @@ def connectaccount():
             'email': session['email'],
             'sl_userid': uID,
             'sourceType': "Google Analytics",
+            'dataSourceName': nForm.data_source_name.data,
             'accountID': nForm.account.data.split('\u0007')[0],
             'accountName': nForm.account.data.split('\u0007')[1],
             'propertyID': nForm.property.data.split('\u0007')[0],
