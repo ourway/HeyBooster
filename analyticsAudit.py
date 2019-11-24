fbefc878523a041cdbf01170675375e93452bca0
altuntasmuhammet <altuntasmuhammet96@gmail.com>
2019-11-25 01:53:59 +0300

    resp['ts'] is removed in analyticsAudit

--- analyticsAudit.py
+++ analyticsAudit.py
@@ -380,7 +380,7 @@ def analyticsAudit(slack_token, task, dataSource, sendFeedback=False):
                 except Exception as error:
                     logging.error(f"SLACK SCHEDULE MESSAGE FAILED --- User Email: {dataSource['email']} Data Source ID: {dataSource['_id']} Task Type: Analytics Audit --- {str(error)}")
                     time.sleep((2 ** n) + random.random())
-        return resp['ts']
+#        return resp['ts']
 
 
 def bounceRateTracking(slack_token, dataSource):
