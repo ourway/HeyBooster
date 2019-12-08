2a64f984ad3ff0f8e144b99b952a2ca0d3bf0043
Farsheed Ashouri <farsheed.ashouri@gmail.com>
2019-12-08 19:39:41 +0300

    Added totalScore

--- app.py
+++ app.py
@@ -115,7 +115,107 @@ def base():
 
 @app.route('/new_theme_index', methods=['GET', 'POST'])
 def new_theme_index():
-    return render_template('new_theme/index.html')
+    # if not session['ga_accesstoken']:
+    #     return redirect('/getstarted/connect-accounts')
+
+    user = db.find_one('user', {'email': session['email']})
+    # tz_offset = user['tz_offset']
+    # tz_offset = 1
+    #    try:
+    #        if user['ga_accesstoken']:
+    #            resp = requests.get(TOKEN_INFO_URI.format(user['ga_accesstoken'])).json()
+    #            if 'error' in resp.keys():
+    #                data = [('client_id', CLIENT_ID.strip()),
+    #                        ('client_secret', CLIENT_SECRET.strip()),
+    #                        ('refresh_token', user['ga_refreshtoken']),
+    #                        ('grant_type', 'refresh_token')]
+    #                resp = requests.post(ACCESS_TOKEN_URI, data).json()
+    #            current_analyticsemail = resp['email']
+    #    except:
+    #        current_analyticsemail = False
+    try:
+        current_analyticsemail = user['ga_email']
+    except:
+        current_analyticsemail = ""
+
+    nForm = DataSourceForm(request.form)
+    datasources = db.find('datasource', query={'email': session['email']})
+    unsortedargs = []
+    for datasource in datasources:
+        unsortedargs.append(datasource)
+    if request.method == 'POST':
+        #        uID = db.find_one("user", query={"email": session["email"]})['sl_userid']
+        #        local_ts = time.asctime(time.localtime(ts))
+        ts = time.time()
+
+        data = {
+            'email': session['email'],
+            'sourceType': "Google Analytics",
+            'dataSourceName': nForm.data_source_name.data,
+            'accountID': nForm.account.data.split('\u0007')[0],
+            'accountName': nForm.account.data.split('\u0007')[1],
+            'propertyID': nForm.property.data.split('\u0007')[0],
+            'propertyName': nForm.property.data.split('\u0007')[1],
+            'viewID': nForm.view.data.split('\u0007')[0],
+            'currency': nForm.view.data.split('\u0007')[1],
+            'viewName': nForm.view.data.split('\u0007')[2],
+            'channelType': "Web",
+            'createdTS': ts
+        }
+        _id = db.insert_one("datasource", data=data).inserted_id
+        data['_id'] = _id
+        unsortedargs.append(data)
+        if len(unsortedargs) == 1:
+            insertdefaultnotifications_without_slack(session['email'], userID='',
+                                                     dataSourceID=_id,
+                                                     channelID='', sendWelcome=False)
+            run_analyticsAudit_without_slack.delay(str(data['_id']))
+        else:
+            insertdefaultnotifications_without_slack(session['email'], userID='',
+                                                     dataSourceID=_id,
+                                                     channelID='')
+            run_analyticsAudit_without_slack.delay(str(data['_id']))
+
+        #        analyticsAudit(slack_token, task=None, dataSource=data)
+        # flash("Check out your connected slack channel, heybooster even wrote you.")
+
+        #        analyticsAudit(slack_token, task=None, dataSource=data)
+
+        flash("Check out your connected slack channel, heybooster even wrote you.")
+
+    useraccounts = google_analytics.get_accounts(session['email'])['accounts']
+    if (useraccounts):
+        nForm.account.choices += [(acc['id'] + '\u0007' + acc['name'], acc['name']) for acc in
+                                  useraccounts]
+    else:
+        nForm.account.choices = [('', 'User does not have Google Analytics Account')]
+        nForm.property.choices = [('', 'User does not have Google Analytics Account')]
+        nForm.view.choices = [('', 'User does not have Google Analytics Account')]
+
+    args = sorted(unsortedargs, key=lambda i: i['createdTS'], reverse=False)
+
+    # Sort Order is important, that's why analytics audits are queried
+    # after sorting to use their status correctly
+    analytics_audits = []
+    for arg in args:
+        #        analytics_audit = db.find_one('notification', query={"datasourceID": arg['_id'], "type": "analyticsAudit"})
+        #        localTime = Timestamp2Date(analytics_audit['lastRunDate'], tz_offset)
+        #        arg['localTime'] = localTime
+        #        if analytics_audit['status'] == '0':
+        #            arg['strstat'] = 'passive'
+        #        else:
+        #            arg['strstat'] = 'active'
+        #        arg['totalScore'] = analytics_audit['totalScore']
+        analytics_audit = db.find_one('notification', query={"datasourceID": arg['_id'], "type": "analyticsAudit"})
+        # analytics_audit['localTime'] = Timestamp2Date(analytics_audit['lastRunDate'], tz_offset)
+        if analytics_audit['status'] == '0':
+            analytics_audit['strstat'] = 'passive'
+        else:
+            analytics_audit['strstat'] = 'active'
+        analytics_audits += [analytics_audit]
+    return render_template('new_theme/index.html', args=args, selectedargs=args, nForm=nForm,
+                           current_analyticsemail=current_analyticsemail,
+                           analytics_audits=analytics_audits)
 
 
 @app.route('/new_theme_widgets', methods=['GET', 'POST'])
