78a304862341e8352a2a7b5dfc498d99d8d386b4
Farsheed Ashouri <farsheed.ashouri@gmail.com>
2019-12-09 01:08:00 +0300

    debug mode

--- app.py
+++ app.py
@@ -250,19 +250,20 @@ def getaudit_without_slack():
     if not session['email']:
         return redirect('/getstarted/connect-accounts')
 
-    counter = 0
-    dt = db.find('datasource', {'email': 'admin@dioscuritechnologies.com'})
-    datasources = db.find('datasource', query={'email': session['email']})
-    user = db.find_one('user', {'email': session['email']})
+    else:
+        counter = 0
+        dt = db.find('datasource', {'email': 'admin@dioscuritechnologies.com'})
+        datasources = db.find('datasource', query={'email': session['email']})
+        user = db.find_one('user', {'email': session['email']})
 
-    for document in dt:
-        print('print1------------', counter)
-        if document['email']:
-            counter = counter + 1
-            print('Print2--------------', counter)
-            if counter > 1:
-                print('Print3-------------------', counter)
-                return redirect('/account/audit-history-without-slack' + datasources['_id'])
+        for document in dt:
+            print('print1------------', counter)
+            if document['email']:
+                counter = counter + 1
+                print('Print2--------------', counter)
+                if counter > 1:
+                    print('Print3-------------------', counter)
+                    return redirect('/account/audit-history-without-slack' + datasources['_id'])
 
     try:
         current_analyticsemail = user['ga_email']
