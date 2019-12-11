40a3f3cad6511dd86ca33676100d500547e03636
Farsheed Ashouri <farsheed.ashouri@gmail.com>
2019-12-11 18:22:43 +0300

    edited url redirect

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ def home():
     current_analyticsemail = ""
     if 'auth_token' in session.keys():
         try:
-            if session['ga_accesstoken'] or session['sl_accesstoken']:
+            if session['ga_accesstoken'] and session['sl_accesstoken']:
                 return redirect('/account/audit-history')
             else:
                 # Check if user has slack connection
